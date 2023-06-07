27113a9ecf21cb2a358dc1bff019808806b3440c
yashpatel259 <118911576+yashpatel259@users.noreply.github.com>
2023-06-06 22:07:37 -0400

    ErrorMaster changes

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,6 +2,9 @@ namespace GlamourHub.Models
 {
     public class ErrorViewModel
     {
+        /// <summary>
+        /// This is for first Sync
+        /// </summary>
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
